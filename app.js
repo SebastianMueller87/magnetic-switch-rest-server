f0e56f09e2d05530e183c8181fedbaa34ae6cd2f
Sebastian Müller <sebastian@sourceboat.com>
2018-01-26 18:57:12 +0100

    Add onoff package and remove pi-gpio

--- app.js
+++ app.js
@@ -3,52 +3,33 @@ var app = express()
 var path = require("path")
 var dotenv = require('dotenv').config({ path: './.env' }).parsed
 var dns = require('dns')
-var gpio = require("pi-gpio")
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   console.log('index route')
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+})
 
 app.get('/blink', function (req, res) {
   console.log('blink route')
-  toggle(16)
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+  watchButton()
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+})
 
 app.listen(dotenv.PORT, function () {
   console.log('Server started')
   dns.lookup(require('os').hostname(), function (err, add, fam) {
     console.log(`Listening on ${add}:${dotenv.PORT}`)
   })
-});
+})
 
-function toggle(pin) {
-  if (!opened) {
-    gpio.open(pin, "input", function (err) {
-    console.log('GPIO: ' + pin + ' opened')
-    opened = true
-    togglePin(pin)
-    })
-  } else {
-    togglePin(pin)
-  }
+function watchButton() {
+  var Gpio = require('onoff').Gpio
+  var led = new Gpio(16, 'out')
+  var button = new Gpio(4, 'in', 'both')
 
-  res.send('Pin ' + pin + ' should be toggled')
-}
-
-function togglePin(pin) {
-  gpio.read(pin, function(err, value) {
-    console.log('GPIO: ' + pin + ' is currently ' + value)
-    gpio.setDirection(pin, 'output', function(err) {
-      console.log('GPIO: ' + pin + ' changed to output')
-      const newValue = value === 0 ? 1 : 0
-      gpio.write(pin, newValue, function() {
-          console.log('GPIO: ' + pin + ' wrote ' + newValue)
-          // gpio.close(pin)
-      })
-    })
+  button.watch(function(err, value) {
+    led.writeSync(value)
   })
 }
