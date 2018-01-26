3e3f78472219230ed3fa2f9b0e2c0d87638945cc
Sebastian Müller <sebastian@sourceboat.com>
2018-01-26 18:28:21 +0100

    Add basic server and gpio toggle

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
-var express = require('express');
-var app = express();
-var path = require("path");
+var express = require('express')
+var app = express()
+var path = require("path")
 var dotenv = require('dotenv').config({ path: './.env' }).parsed
+var dns = require('dns')
+var gpio = require("pi-gpio")
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
+  console.log('index route')
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/blink', function (req, res) {
+  console.log('blink route')
+  toggle(16)
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(dotenv.PORT, function () {
-  console.log('Server listening on port ' + dotenv.PORT + '!');
+  console.log('Server started')
+  dns.lookup(require('os').hostname(), function (err, add, fam) {
+    console.log(`Listening on ${add}:${dotenv.PORT}`)
+  })
 });
+
+function toggle(pin) {
+  if (!opened) {
+    gpio.open(pin, "input", function (err) {
+    console.log('GPIO: ' + pin + ' opened')
+    opened = true
+    togglePin(pin)
+    })
+  } else {
+    togglePin(pin)
+  }
+
+  res.send('Pin ' + pin + ' should be toggled')
+}
+
+function togglePin(pin) {
+  gpio.read(pin, function(err, value) {
+    console.log('GPIO: ' + pin + ' is currently ' + value)
+    gpio.setDirection(pin, 'output', function(err) {
+      console.log('GPIO: ' + pin + ' changed to output')
+      const newValue = value === 0 ? 1 : 0
+      gpio.write(pin, newValue, function() {
+          console.log('GPIO: ' + pin + ' wrote ' + newValue)
+          // gpio.close(pin)
+      })
+    })
+  })
+}
