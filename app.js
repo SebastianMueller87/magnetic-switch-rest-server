e87028aea03ac32116f6fc284ed646c18f5c7b64
Sebastian Müller <sebastian@sourceboat.com>
2018-01-27 11:18:05 +0100

    Add json for status change storage

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const dns = require('dns')
 const Gpio = require('onoff').Gpio
 const magnetPinNumber = 4
 const ledPinNumber = 16
-
+const jsonfile = require('jsonfile')
+const jsonDataFile = './public/data.json'
 
 /**
  * ################################
@@ -18,6 +19,13 @@ app.get('/', function (req, res) {
   console.log('index route')
   res.sendFile(path.join(__dirname + '/public/index.html'))
 })
+
+app.get('/store', function (req, res) {
+  console.log('store route')
+  storeToJson(0)
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+})
+
 
 app.listen(dotenv.PORT, function () {
   console.log('Server started')
@@ -41,5 +49,30 @@ function initWatcher() {
   magnetSensorPin.watch(function(err, value) {
     console.log('new Value: ', value)
     ledPin.writeSync(value)
+    storeToJson(value)
+  })
+}
+
+/**
+ * ################################
+ *  Store
+ */
+function storeToJson(status) {
+  jsonfile.readFile(jsonDataFile, (err, obj) => {
+    if (err) {
+      console.log('Error while reading settings', err)
+      return
+    }
+
+    obj[Date.now()] = status
+
+    jsonfile.writeFile(jsonDataFile, obj, {spaces: 2}, (err) => {
+      if (err) {
+        console.log('Error while writing settings', err)
+        return
+      }
+
+      console.log('Data stored');
+    })
   })
 }
